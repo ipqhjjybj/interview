880f261e859dc2a92b87f51b8879de5490c29406
ipqhjjybj <250657661@qq.com>
2023-12-12 12:53:16 +0800

    add code

--- interviewer/知识点/低延时/test.cpp
+++ interviewer/知识点/低延时/test.cpp
@@ -1,12 +1,17 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 struct priceVolume{
 	string price;
 	int volume;
 	priceVolume(){volume=0;}
+	priceVolume(string _price, int _volume){
+		price = _price;
+		volume = _volume;
+	}
 
 	void print(){
 		cout << "price:" << price << endl;
@@ -16,12 +21,18 @@ struct priceVolume{
 
 struct depthResult{
 	string U;
-	priceVolume bids, asks;
+	vector<priceVolume> bids, asks;
 	void print(){
 		cout << "bids:" << endl;
-		bids.print();
+		for(int i = 0;i < bids.size(); i++){
+			cout << "priceVolume:" << i << ": " ;
+			bids[i].print();	
+		}
 		cout << "asks:" << endl;
-		asks.print();
+		for(int i = 0;i < asks.size(); i++){
+			cout << "priceVolume:" << i << ": " ;
+			asks[i].print();	
+		}
 	}
 };
 
@@ -54,11 +65,12 @@ struct depthUpdate parseDepthUpdate(string line);
 int main(){
 	string line;
 	
-	cin >> line;
-	cout << line << endl;
+	while(cin >> line){
+		cout << line << endl;
 
-	depthUpdate ans = parseDepthUpdate(line);
-	//ans.print();
+		depthUpdate ans = parseDepthUpdate(line);
+		ans.print();
+	}
 	
 	// while(cin >> line){
 	// 	cout << line << endl;
@@ -83,16 +95,52 @@ string getStrIntgerValueFromKey(string s, string key){
 	string result = s.substr(pos + n + 2);
 	int end_pos = result.find(",");
 	result = result.substr(0, end_pos);
+	//cout << "result size:" << result.size() << endl;
+	if(result.size() > 0 && result[result.size()-1] == '}'){
+		result = result.substr(0, result.size()-1);
+	}
 	//cout << result << endl;
 	return result;
+}
+
+vector<string> getArrayValueFromKey(string s, string key){
+	vector<string> ans;
+	int pos = s.find("\"" + key + "\"");
+	s = s.substr(pos);
+	int left_find = s.find("[");
+	int right_find = s.find("]");
+	string arrayinfo = s.substr(left_find, right_find - left_find + 1);
+
+	vector<int> cc;
+	for(int i = 0;i < arrayinfo.size();i++){
+		// && i + 1 < arrayinfo.size() && arrayinfo[i-1] == '{'
+		if(arrayinfo[i] == ',' && i > 0 && arrayinfo[i-1] == '}' ){
+			cc.push_back(i);
+		}
+	}
+	cc.push_back(arrayinfo.size() - 1);
+
+	int left = 1;
+	for(int i = 0; i < cc.size();i++){
+		int ns = cc[i];
+		// printf("i=%d ns=%d\n", i, ns);
+		string line = arrayinfo.substr(left, ns - left );
+		// cout << "line:" << line << endl;
+		if(line.size() > 0){
+			ans.push_back(line);
+		}
+		left = ns+1;
+	}
+	return ans;
 }
 
 struct depthUpdate parseDepthUpdate(string line){
 	depthUpdate data;
 	int pos = line.find("\"result\"");
-	printf("pos=%d\n", pos);
+	// printf("pos=%d\n", pos);
 	string left_line = line.substr(0, pos);
 	string right_line = line.substr(pos);
+	
 	cout << "left_line:" << left_line << endl;
 
 	data.time = atoi(left_line.substr(8, 10).c_str());
@@ -105,22 +153,36 @@ struct depthUpdate parseDepthUpdate(string line){
 	cout << "channel:" << data.channel << endl;
 	cout << "event:" << data.event << endl;
 
-	//cout << "right_line:" << right_line << endl;
+	cout << "right_line:" << right_line << endl;
 
 	int mid_s = right_line.find("\"s\":\"");
 	string mid_line = right_line.substr(0, mid_s);
 	string end_line = right_line.substr(mid_s);
 
 	data.result.U = getStrIntgerValueFromKey(mid_line, "U");
-	cout << "data.result.U: " << data.result.U << endl;
-	cout << "mid_line:" << mid_line << endl;
-	cout << "end_line:" << end_line << endl;
+	// cout << "data.result.U: " << data.result.U << endl;
+	// cout << "mid_line:" << mid_line << endl;
+	// cout << "end_line:" << end_line << endl;
+
+	vector<string> a_vector = getArrayValueFromKey(mid_line, "a");
+	for(int i = 0;i < a_vector.size();i++){
+		string ask_price = getStrValueFromKey(a_vector[i], "p");
+		int ask_sell_number = atoi(getStrIntgerValueFromKey(a_vector[i], "s").c_str());
+
+		data.result.bids.push_back(priceVolume(ask_price, ask_sell_number));
+	}
+
+	vector<string> b_vector = getArrayValueFromKey(mid_line, "b");
+	for(int i = 0;i < b_vector.size();i++){
+		string bid_price = getStrValueFromKey(b_vector[i], "p");
+		int bid_buy_number = atoi(getStrIntgerValueFromKey(b_vector[i], "s").c_str());
+		data.result.asks.push_back(priceVolume(bid_price, bid_buy_number));
+	}
 
 	string real_symbol = end_line.substr(5);
 	int tpos = real_symbol.find("\",\"t");
 	data.symbol = real_symbol.substr(0, tpos);
 	
-
 	//cout << "real_symbol:" << real_symbol << endl;
 	// char symbol[20];
 	// sscanf("\"s\":\"BTC_USDT\",\"t\":1701245639006,\"u\":53095233724}}", "\"s\":\"%s\",\"t\":%d,\"u\":%d}}", symbol, &data.t, &data.u);
--- interviewer/知识点/低延时/test2.cpp
+++ interviewer/知识点/低延时/test2.cpp
@@ -1,16 +1,93 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 string ori="{\"time\":1701245639,\"time_ms\":1701245639014,\"channel\":\"futures.order_book_update\",\"event\":\"update\",";
 string getStrValueFromKey(string s, string key);
 
+//string ori2="\"a\":[{\"p\":\"38155.9\",\"s\":0},{\"p\":\"38154.5\",\"s\":0},{\"p\":\"38154.5\",\"s\":0}],";
+string ori2="\"b\":[],\"s\":\"BTC_USDT\"";
+vector<string> getArrayValueFromKey(string s, string key){
+	vector<string> ans;
+	int pos = s.find("\"" + key + "\"");
+	s = s.substr(pos);
+	int left_find = s.find("[");
+	int right_find = s.find("]");
+	string arrayinfo = s.substr(left_find, right_find - left_find + 1);
+	vector<int> cc;
+	for(int i = 0;i < arrayinfo.size();i++){
+		// && i + 1 < arrayinfo.size() && arrayinfo[i-1] == '{'
+		if(arrayinfo[i] == ',' && i > 0 && arrayinfo[i-1] == '}' ){
+			cc.push_back(i);
+		}
+	}
+	cc.push_back(arrayinfo.size() - 1);
+
+	int left = 1;
+	for(int i = 0; i < cc.size();i++){
+		int ns = cc[i];
+		//printf("i=%d ns=%d\n", i, ns);
+		string line = arrayinfo.substr(left, ns - left );
+		if(line.size() > 0){
+			ans.push_back(line);
+		}
+		left = ns+1;
+	}
+	return ans;
+}
+
+
+struct priceVolume{
+	string price;
+	int volume;
+	priceVolume(){volume=0;}
+
+	void print(){
+		cout << "price:" << price << endl;
+		cout << "volume:" << volume << endl;
+	}
+
+};
+
+string getStrIntgerValueFromKey(string s, string key);
+string getStrValueFromKey(string s, string key);
+
 int main(){
-	string result = getStrValueFromKey(ori, "channel");
+	// {"p":"38155.9","s":0}
+
+	//string result = getStrValueFromKey(ori, "channel");
+	cout << "debug 1" << endl;
+	vector<string> lines = getArrayValueFromKey(ori2, "b");
+	cout << "debug 2" << endl;
+	vector<priceVolume> vecPriceVolume;
+	for(int i = 0;i < lines.size();i++){
+		cout << "line:" << lines[i] << " why!!" << endl;
+		string ask_price = getStrValueFromKey(lines[i], "p");
+		string ask_sell_number = getStrIntgerValueFromKey(lines[i], "s");
+
+		cout << "ask_price:" << ask_price << endl;
+		cout << "ask_sell_number:" << ask_sell_number << endl;
+	}
 
 	return 0;
 }
+
+string getStrIntgerValueFromKey(string s, string key){
+	int pos = s.find(key + "\"");
+	int n = key.size();
+	string result = s.substr(pos + n + 2);
+	int end_pos = result.find(",");
+	result = result.substr(0, end_pos);
+	//cout << "result size:" << result.size() << endl;
+	if(result.size() > 0 && result[result.size()-1] == '}'){
+		result = result.substr(0, result.size()-1);
+	}
+	//cout << result << endl;
+	return result;
+}
+
 
 string getStrValueFromKey(string s, string key){
 	int pos = s.find(key + "\"");
